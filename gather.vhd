15eaae8b757573c715d92517cadf46002fee8cdb
Caroline Collange <caroline.collange@inria.fr>
2016-02-01 20:58:28 +0100

    Build files and sub-word loads

--- gather.vhd
+++ gather.vhd
@@ -28,15 +28,30 @@ entity Gather is
 end entity;
 
 architecture structural of Gather is
-	signal broadcast_word : scalar;
+	signal broadcast_word_raw, broadcast_word_mux0, broadcast_word : scalar;
 	signal leader_offset_word_int : integer;
 	signal memwriteback_0 : vector;
+	signal sign_extend : std_logic;
 begin
 	-- Access offset within cache block
 	leader_offset_word_int <= to_integer(unsigned(leader_offset(log_blocksize - 1 downto 2)));
-	broadcast_word <= data_block((leader_offset_word_int + 1) * 32 - 1 downto leader_offset_word_int * 32);
+	broadcast_word_raw <= data_block((leader_offset_word_int + 1) * 32 - 1 downto leader_offset_word_int * 32);
 	
-	-- TODO: Shift/mux broadcast word for sub-word access
+	-- Shift/mux broadcast word for sub-word access
+	sign_extend <= insn_in.mem_size(2);
+	broadcast_word_mux0(15 downto 0) <= broadcast_word_raw(31 downto 16) when leader_offset(1) = '1' else
+	                                    broadcast_word_raw(15 downto 0);
+	--broadcast_word_mux0(31 downto 15) <= (others => broadcast_word_raw(15) and sign_extend) when leader_offset(1) = '1' else
+	--                                     broadcast_word_raw(31 downto 15);
+	
+	broadcast_word(7 downto 0) <= broadcast_word_mux0(15 downto 8) when leader_offset(0) = '1' else
+	                              broadcast_word_mux0(7 downto 0);
+	-- Sign- or Zero-extend on sub-word access
+	broadcast_word(15 downto 8) <= (others => broadcast_word(7) and sign_extend) when insn_in.mem_size(1 downto 0) = "00" else
+	                               broadcast_word_mux0(15 downto 8);
+	broadcast_word(31 downto 15) <= (others => broadcast_word_mux0(15) and sign_extend) when insn_in.mem_size(1 downto 0) = "01" else
+	                                (others => broadcast_word(7) and sign_extend) when insn_in.mem_size(1 downto 0) = "00" else
+	                                broadcast_word_raw(31 downto 15);
 
 	data_mux: for i in 0 to warpsize - 1 generate
 		memwriteback_0((i+1)*32-1 downto i*32) <=
